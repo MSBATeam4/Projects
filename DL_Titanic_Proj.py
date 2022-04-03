0175572d0ec3f604061ac612e5b4c0982fab686e
MSBATeam4 <98135424+MSBATeam4@users.noreply.github.com>
2022-04-02 22:34:07 -0400

    Added Age Bins

--- DL_Titanic_Proj.py
+++ DL_Titanic_Proj.py
@@ -48,6 +48,14 @@ titanic.drop(['Wealthy_Pclass', 'Wealthy_Fare'], axis=1, inplace = True)
 titanic['ThirdClass_Cabin'] = np.logical_and(titanic['Pclass'] == 3, titanic['Cabin_ind'] == 1)
 titanic['ThirdClass_Cabin'] = np.where((titanic['ThirdClass_Cabin'] == False), 0, 1)
 
+#Age Bins
+titanic['Child'] = np.where((titanic['Age'] <=15), 1, 0)
+titanic['Young_Adult'] = np.logical_and(titanic['Age'] >15, titanic['Age']<=29)
+titanic['Young_Adult'] = np.where((titanic['Young_Adult'] == False), 0, 1)
+titanic['Middle_Aged'] = np.logical_and(titanic['Age']>29, titanic['Age']<=54)
+titanic['Middle_Aged'] = np.where((titanic['Middle_Aged'] == False), 0, 1)
+titanic['Senior'] = np.where((titanic['Age']>54), 1, 0)
+
 
 #Convert Sex to numeric, notice use of dictionary where in R we would use ifelse
 gender_num = {'male': 0, 'female': 1}
