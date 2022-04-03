daef14b255a30824c5f427cccc616407e746763a
MSBATeam4 <98135424+MSBATeam4@users.noreply.github.com>
2022-04-03 11:42:29 -0400

    Added Age based cheap tickets

--- DL_Titanic_Proj.py
+++ DL_Titanic_Proj.py
@@ -60,6 +60,16 @@ titanic['Senior'] = np.where((titanic['Age']>54), 1, 0)
 titanic['Cheap_Cabin'] = np.logical_and(titanic['Fare'] < Fare_Avg, titanic['Cabin_ind'] == 1)
 titanic['Cheap_Cabin'] = np.where((titanic['Cheap_Cabin'] == False), 0, 1)
 
+#Cheap tickets based on Age
+titanic['Age_Fare_Child'] = np.logical_and(titanic['Child'] == 1, titanic['Fare'] < Fare_Avg)
+titanic['Age_Fare_Child'] = np.where((titanic['Age_Fare_Child'] == False), 0, 1)
+titanic['Age_Fare_YA'] = np.logical_and(titanic['Young_Adult'] == 1, titanic['Fare'] < Fare_Avg)
+titanic['Age_Fare_YA'] = np.where((titanic['Age_Fare_YA'] == False), 0, 1)
+titanic['Age_Fare_MA'] = np.logical_and(titanic['Middle_Aged'] == 1, titanic['Fare'] < Fare_Avg)
+titanic['Age_Fare_MA'] = np.where((titanic['Age_Fare_MA'] == False), 0, 1)
+titanic['Age_Fare_Senior'] = np.logical_and(titanic['Senior'] == 1, titanic['Fare'] < Fare_Avg)
+titanic['Age_Fare_Senior'] = np.where((titanic['Age_Fare_Senior'] == False), 0, 1)
+
 
 #Convert Sex to numeric, notice use of dictionary where in R we would use ifelse
 gender_num = {'male': 0, 'female': 1}
